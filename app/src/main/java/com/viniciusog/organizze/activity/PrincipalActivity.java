f20c6ce38ec8e7363285145735e3327e23ea13d4
Viniciusog <vinijipa23@gmail.com>
2019-09-23 18:38:18 -0400

    configure style

--- app/src/main/java/com/viniciusog/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/viniciusog/organizze/activity/PrincipalActivity.java
@@ -1,5 +1,6 @@
 package com.viniciusog.organizze.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -31,6 +32,14 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });
         */
+    }
+
+    public void adicionarReceita(View view) {
+        startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void adicionarDespesa(View view){
+        startActivity(new Intent(this, DespesasActivity.class));
     }
 
 }
